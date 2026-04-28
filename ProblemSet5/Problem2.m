ab8022a6a2280f9675a04c6f80540f8410b1e6d3
Kaitlyn Tung <109412585+kaitlyntung@users.noreply.github.com>
2026-04-28 18:31:12 -0500

    part A

--- ProblemSet5/Problem2.m
+++ ProblemSet5/Problem2.m
@@ -1,2 +1,27 @@
 %% Part 1
-% Part A
+%% Part A
+nConditions = length(R);
+nUnits  = length(R(1).unit);
+startTime = -150;
+endTime = 500;
+binSize = 10;
+bin_edges = (startTime:binSize:endTime);
+nTimeBins = length(bin_edges) - 1; 
+spikeCounts = zeros(nTimeBins, nConditions, nUnits);
+
+for trial = 1:nTrials
+    moveOnsetTime = R(trial).moveOnsetTime;
+    for unit = 1:nUnits
+        spikeTime = R(trial).unit(unit).spikeTimes;
+        if isempty(spikeTime)
+            continue
+        end
+        spikeTimeLocked = spikeTime - moveOnsetTime;
+        spikeCounts(:, trial, unit) = histcounts(spikeTimeLocked, bin_edges);
+    end
+end
+
+%% Part B
+out = gaussFilt1(spikeCounts, 40);
+
+%% Part C
