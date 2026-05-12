7985b1e819a1da6486e1c8b8d21f5f78f04a6df1
Kaitlyn Tung <109412585+kaitlyntung@users.noreply.github.com>
2026-05-12 10:34:16 -0500

    updates

---
+++ FinalProject/Scripts/handVelocityMonteCarlo.m
@@ -0,0 +1,7 @@
+% Behavioral: Hand velocity over time split by the curved and straight 
+% reaches. Compute peak hand speed during trial, split trials into groups 
+% based on curved or straight reaches, compare the means. Monte Carlo: 
+% shuffle the curved/straight labels, recompute the difference in the 
+% means, do this 1,000 times to create a null distribution. P-value would 
+% show us if curved and straight reaches have significantly different peak 
+% hand speeds. 
